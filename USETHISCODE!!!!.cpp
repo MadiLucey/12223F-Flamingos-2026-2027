264ed260915d4929bcd4b2150101ebfa31367720
Madi <pinkdraggle@gmail.com>
2026-07-22 12:02:47 -0400

    Should build (hopefully)

--- USETHISCODE!!!!.cpp
+++ USETHISCODE!!!!.cpp
@@ -27,24 +27,15 @@ brain Brain;
 
 
 // Robot configuration code.
-motor leftMotorA = motor(PORT17, ratio18_1, false);
-motor leftMotorB = motor(PORT14, ratio18_1, false);
-motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
-motor rightMotorA = motor(PORT4, ratio18_1, true);
-motor rightMotorB = motor(PORT19, ratio18_1, true);
-motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
-inertial DrivetrainInertial = inertial(PORT1);
-smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 319.19, 320, 40, mm, 1);
-
-// AI Vision Color Descriptions
-// AI Vision Code Descriptions
-vex::aivision AIVision11(PORT11, aivision::ALL_TAGS);
-
-motor Claw = motor(PORT3, ratio18_1, false);
-
-motor arm = motor(PORT5, ratio18_1, false);
-
 controller Controller1 = controller(primary);
+motor Left_Front = motor(PORT17, ratio18_1, false);
+
+motor Right_Front = motor(PORT14, ratio18_1, false);
+
+motor Left_Back = motor(PORT4, ratio18_1, false);
+
+motor Right_Back = motor(PORT19, ratio18_1, false);
+
 
 
 // generating and setting random seed
@@ -60,26 +51,9 @@ void initializeRandomSeed(){
   srand(seed);
 }
 
-bool vexcode_initial_drivetrain_calibration_completed = false;
-void calibrateDrivetrain() {
-  wait(200, msec);
-  Brain.Screen.print("Calibrating");
-  Brain.Screen.newLine();
-  Brain.Screen.print("Inertial");
-  DrivetrainInertial.calibrate();
-  while (DrivetrainInertial.isCalibrating()) {
-    wait(25, msec);
-  }
-  vexcode_initial_drivetrain_calibration_completed = true;
-  // Clears the screen and returns the cursor to row 1, column 1.
-  Brain.Screen.clearScreen();
-  Brain.Screen.setCursor(1, 1);
-}
+
 
 void vexcodeInit() {
-
-  // Calibrate the Drivetrain
-  calibrateDrivetrain();
 
   //Initializing random seed.
   initializeRandomSeed(); 
@@ -94,105 +68,8 @@ void playVexcodeSound(const char *soundName) {
 }
 
 
-
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
-// define variables used for controlling motors based on controller inputs
-bool Controller1LeftShoulderControlMotorsStopped = true;
-bool Controller1RightShoulderControlMotorsStopped = true;
-bool DrivetrainLNeedsToBeStopped_Controller1 = true;
-bool DrivetrainRNeedsToBeStopped_Controller1 = true;
-
-// define a task that will handle monitoring inputs from Controller1
-int rc_auto_loop_function_Controller1() {
-  // process the controller input every 20 milliseconds
-  // update the motors based on the input values
-  while(true) {
-    if(RemoteControlCodeEnabled) {
-      // stop the motors if the brain is calibrating
-      if (DrivetrainInertial.isCalibrating()) {
-        LeftDriveSmart.stop();
-        RightDriveSmart.stop();
-        while (DrivetrainInertial.isCalibrating()) {
-          wait(25, msec);
-        }
-      }
-      
-      // calculate the drivetrain motor velocities from the controller joystick axies
-      // left = Axis3
-      // right = Axis2
-      int drivetrainLeftSideSpeed = Controller1.Axis3.position();
-      int drivetrainRightSideSpeed = Controller1.Axis2.position();
-      
-      // check if the value is inside of the deadband range
-      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
-        // check if the left motor has already been stopped
-        if (DrivetrainLNeedsToBeStopped_Controller1) {
-          // stop the left drive motor
-          LeftDriveSmart.stop();
-          // tell the code that the left motor has been stopped
-          DrivetrainLNeedsToBeStopped_Controller1 = false;
-        }
-      } else {
-        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
-        DrivetrainLNeedsToBeStopped_Controller1 = true;
-      }
-      // check if the value is inside of the deadband range
-      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
-        // check if the right motor has already been stopped
-        if (DrivetrainRNeedsToBeStopped_Controller1) {
-          // stop the right drive motor
-          RightDriveSmart.stop();
-          // tell the code that the right motor has been stopped
-          DrivetrainRNeedsToBeStopped_Controller1 = false;
-        }
-      } else {
-        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
-        DrivetrainRNeedsToBeStopped_Controller1 = true;
-      }
-      
-      // only tell the left drive motor to spin if the values are not in the deadband range
-      if (DrivetrainLNeedsToBeStopped_Controller1) {
-        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
-        LeftDriveSmart.spin(forward);
-      }
-      // only tell the right drive motor to spin if the values are not in the deadband range
-      if (DrivetrainRNeedsToBeStopped_Controller1) {
-        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
-        RightDriveSmart.spin(forward);
-      }
-      // check the ButtonL1/ButtonL2 status to control Claw
-      if (Controller1.ButtonL1.pressing()) {
-        Claw.spin(forward);
-        Controller1LeftShoulderControlMotorsStopped = false;
-      } else if (Controller1.ButtonL2.pressing()) {
-        Claw.spin(reverse);
-        Controller1LeftShoulderControlMotorsStopped = false;
-      } else if (!Controller1LeftShoulderControlMotorsStopped) {
-        Claw.stop();
-        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
-        Controller1LeftShoulderControlMotorsStopped = true;
-      }
-      // check the ButtonR1/ButtonR2 status to control arm
-      if (Controller1.ButtonR1.pressing()) {
-        arm.spin(forward);
-        Controller1RightShoulderControlMotorsStopped = false;
-      } else if (Controller1.ButtonR2.pressing()) {
-        arm.spin(reverse);
-        Controller1RightShoulderControlMotorsStopped = false;
-      } else if (!Controller1RightShoulderControlMotorsStopped) {
-        arm.stop();
-        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
-        Controller1RightShoulderControlMotorsStopped = true;
-      }
-    }
-    // wait before repeating the process
-    wait(20, msec);
-  }
-  return 0;
-}
-
-task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
 
 #pragma endregion VEXcode Generated Robot Configuration
 
